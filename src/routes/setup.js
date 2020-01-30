4e4081b7109954038c9c6a40fbcd56972044c1df
lucatk <lucakillmaier@lu-co.de>
2020-01-30 20:14:50 +0100

    Route /setup

--- src/routes/setup.js
+++ src/routes/setup.js
@@ -1,3 +1,25 @@
-module.exports = (match, req, res) => {
-  return res.end(JSON.stringify({ url: 'setup', match, query: req.query }));
+const setupHandler = require('../setupHandler');
+const { makeResponse } = require('../utils');
+
+module.exports = async (match, req, res) => {
+  const { service } = req.query;
+  if (!service) {
+    return res.status(400).end();
+  }
+  const setup = setupHandler[service]();
+  if (!setup) {
+    return res.status(400).end();
+  }
+  try {
+    const result = await setup(req.query);
+    return res.json(
+      makeResponse(result)
+    );
+  } catch (error) {
+    return res.status(500).json(
+      makeResponse({
+        error: error
+      })
+    );
+  }
 };
